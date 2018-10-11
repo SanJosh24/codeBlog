84ae7b9f434b3ce659c8cf7063b59e20675169c8
Josh Matos <jmatos.5@aol.com>
2018-10-11 17:12:27 -0400

    blog posting works

--- coDropFrontend/src/components/ProfilePage.js
+++ coDropFrontend/src/components/ProfilePage.js
@@ -6,6 +6,7 @@ class ProfilePage extends Component {
   constructor(props){
     super(props);
     this.state = {
+      title: "", description: ""
        };
   }
 
@@ -20,28 +21,44 @@ class ProfilePage extends Component {
 
   }
 
- 
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    const {title, description} = this.state;
+    axios.post("http://localhost:5000/api/profile/post", {title, description } , {withCredentials: true}) 
+    .then( () => {
+        // this.props.getData();
+        this.setState({title: "", description: ""});
+        this.getAllProfileBlogs();
+        
+    })
+    .catch( error => console.log(error) )
+  }
+
+  handleChange = (event) => {
+    const {name, value} = event.target;
+    this.setState({[name]: value})
+  }
 
   getAllProfileBlogs = () =>{
     axios.get(`http://localhost:5000/api/profile/${this.props.match.params.id}/blogs`)
     .then(responseFromApi => {
-      console.log("this is the response from the axios get -=-=-=-=",responseFromApi.data);
+      // console.log("this is the response from the axios get -=-=-=-=",responseFromApi.data);
 
 
       let othersBlogs = []
 
 
       responseFromApi.data.favoriteUsers.forEach((onePerson)=>{
-        console.log("the first for each 1111111111111111111111", onePerson);
+        // console.log("the first for each 1111111111111111111111", onePerson);
 
         onePerson.blogs.forEach((oneBlog)=>{
-          console.log("the third for each 33333333333333333333333");
+          // console.log("the third for each 33333333333333333333333");
           othersBlogs.push(oneBlog)
         })
-        console.log("the ending of the first for each 444444444444444444");    
+        // console.log("the ending of the first for each 444444444444444444");    
       })
 
-      console.log("the state after the axios call $$$$$$$$$$$$$$$$$$$$$$$$$$$ ", othersBlogs);
+      // console.log("the state after the axios call $$$$$$$$$$$$$$$$$$$$$$$$$$$ ", othersBlogs);
       this.setState({
         theProfile: responseFromApi.data,
         othersBlogs: othersBlogs,
@@ -54,10 +71,10 @@ class ProfilePage extends Component {
 
   showUserBlogs() {
     if(this.state.theProfile) {
-      console.log("the state when trying to show blogs.. ..... ...... .. .... .... .", this.state.theProfile.blogs)
+      // console.log("the state when trying to show blogs.. ..... ...... .. .... .... .", this.state.theProfile.blogs)
       return (
         this.state.theProfile.blogs.map((blog, index) => {
-         return <div key={index}>
+         return <div className= "blogs" key={index}>
             <h3> {blog.title} </h3>
             <p>{blog.description}</p>
           </div>
@@ -68,10 +85,10 @@ class ProfilePage extends Component {
 
   showFavoriteUserBlogs() {
     if(this.state.othersBlogs) {
-      console.log("the state when trying to show favorite users blogs 909090909090909090909", this.state.othersBlogs.blogs);
+      // console.log("the state when trying to show favorite users blogs 909090909090909090909", this.state.othersBlogs.blogs);
       return (
         this.state.othersBlogs.map((blog, index) => {
-          return <div key={index}>
+          return <div className= "favoriteBlogs" key={index}>
             <h3>{blog.title}</h3>
             <p>{blog.description}</p>
           </div>
@@ -85,21 +102,26 @@ class ProfilePage extends Component {
   }
 
   render() {
-      console.log('=this is the state entering profile page',this.state);
+      // console.log('=this is the state entering profile page',this.state);
       // console.log('=this is the props entering profile page $$$$$$$$$$$',this.state.loggedInUser.username);
       return (
         <div className="ProfilePage">
           <div className="profileHead">
             <h1>{this.state.loggedInUser && this.state.loggedInUser.username}'s Profile</h1>
             <h3>Post</h3>
-            <form action="">
+
+            <form onSubmit={this.handleFormSubmit}>
               <label>Blog Title</label>
-              <input type="text"/>
               <br/>
-              <textarea name="" id="" cols="150" rows="10" placeholder="Post a blog"></textarea>
+              <input type="text" name="title" value={this.state.title} onChange={e => this.handleChange(e)}/>
               <br/>
-              <button>Post</button>
+              <label>Blog Description</label>
+              <br/>
+              <input className="description" type="text" name="description" value={this.state.description} onChange={e => this.handleChange(e)} placeholder="Post Description"/>
+              <br/>
+              <input type="submit" value="Submit" />
             </form>
+
           <br/>
 
           {this.showUserBlogs()}
